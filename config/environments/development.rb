3d1a8ed83c162bf852b0026741cdd7c3c549d42e
Maciej Gębka <maciej.gebka@useo.pl>
2015-03-24 14:36:53 +0100

    Mailtrap integration is working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 	# Line required by Devise
-	#config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Mailtrap integration.
   config.action_mailer.delivery_method = :smtp
