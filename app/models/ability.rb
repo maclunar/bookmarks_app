71a643de35094724d634cd8b387c9cb6b161031b
Maciej Gębka <maciej.gebka@useo.pl>
2015-03-26 10:45:40 +0100

    removed permissions to edit domains

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,11 @@ class Ability
   def initialize(user)
     # user ||= User.new # guest user (not logged in)
 
-
     can :read, Bookmark
     can :read, Domain
 
-    can :manage, Domain
-    can :create, Domain
+    # can :manage, Domain
+    # can :create, Domain
 
 
     if user.blank?
@@ -19,31 +18,5 @@ class Ability
       can :manage, Bookmark
     end
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
